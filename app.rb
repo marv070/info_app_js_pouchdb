1b9229243d6184d720da30bc8ce3f4e9e7f08d39
Marvin Laucher <marv070@gmail.com>
2016-12-29 14:56:55 -0500

    combine all docs. Now we can update/add to existing doc.

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ get'/' do
 end
 
 post '/details' do
-  @first_name = params[:first_name]
-  @last_name = params[:last_name]
-  @email = params[:email]
-  gon.first_name = @first_name
-  gon.last_name = @last_name
-  gon.email = @email
-  erb :show_details, :locals => {:first_name => @first_name, :last_name => @last_name, :email => @email}
+  # @first_name = params[:first_name]
+  # @last_name = params[:last_name]
+  # @email = params[:email]
+  # gon.first_name = @first_name
+  # gon.last_name = @last_name
+  # gon.email = @email
+  # erb :show_details, :locals => {:first_name => @first_name, :last_name => @last_name, :email => @email}
 end
--- public/js/info.js
+++ public/js/info.js
@@ -1,77 +1,194 @@
 var db;
-      // Initialize PouchDB database
-      function initDB() {
-        db = new PouchDB("practice_details");
-        db.info(function(error, result) {
-          if(!error) {
-            console.log(result);
-          } else {
-            console.log(error);
-          }
-        });
-        return db;
-      } //END initDB()
+// Initialize PouchDB database
+function initDB() {
+  db = new PouchDB("practice_details");
+  db.info(function(error, result) {
+    if(!error) {
+      console.log(result);
+    } else {
+      console.log(error);
+    }
+  });
+  return db;
+} //END initDB()
       
-      initDB();
+initDB();
+var Time = "1";
       
-      // Functionality to add a user
-      $("#btnAddNames").on("click", addUsers);
-      function addUsers() {         
-        var uFirst   = $("#inputFirst").val(),
-          uLast  = $("#inputLast").val(),
-          uEmail  = $("#inputEmail").val();
-          uTime = new Date().toISOString();
-        var aUser = {
-          "_id"   : uTime,
-          "nameFirst" : uFirst,
-          "nameLast" : uLast,
-          "nameEmail"  : uEmail
-        };
-        
-        db.put(aUser, function(error, result) {
-          if(!error) {
-            $("#divResults").html("<p>Saved!</p>");
-            showUsers();
-            console.log(result);
-            clearFields();
-          } else {
-            $("#divResults").html("<p>&iexcl;Error!</p>");
-            console.log(error);
-          }
-         db.replicate.to('https://minedmindstestdb:minedmindstestdb123@minedmindstestdb.cloudant.com/info_details');
+// Functionality to add a user
+$("#btnAddNames").on("click", addUsers);
+function addUsers() {         
+  var uFirst   = $("#inputFirst").val(),
+    uLast  = $("#inputLast").val(),
+    uEmail  = $("#inputEmail").val();
+    uTime = Time;
+    uLocation = " ";
+  var aUser = {
+    "_id"   : uTime,
+    "nameFirst" : uFirst,
+    "nameLast" : uLast,
+    "nameEmail"  : uEmail,
+    "userLocation" : uLocation,
+  };
+  
+  db.put(aUser, function(error, result) {
+    if(!error) {
+      $("#divResults").html("<p>Saved!</p>");
+      showUsers();
+      console.log(result);
+      clearFields();
+    } else {
+      $("#divResults").html("<p>&iexcl;Error!</p>");
+      console.log(error);
+    }
+   db.replicate.to('https://minedmindstestdb:minedmindstestdb123@minedmindstestdb.cloudant.com/info_details');
  
-        });
-      } //END addUsers();
-      // var remoteCouch = 'https://minedmindstestdb:minedmindstestdb123@minedmindstestdb.cloudant.com/info_details';
+  });
+} //END addUsers();
+// var remoteCouch = 'https://minedmindstestdb:minedmindstestdb123@minedmindstestdb.cloudant.com/info_details';
       
-      // To clear fields
-      function clearFields() {
-        $("#formUser")[0].reset();
-      } //END clearFields()
+// To clear fields
+function clearFields() {
+  $("#formUser")[0].reset();
+} //END clearFields()
       
-      // Get the database of user names ready
-      $("#btnShowNames").on("click", showUsers);
-      function showUsers() {
-        db.allDocs({"include_docs" : true, "ascending" : true}, function(error, result) {
-          showTableOfUsers(result.rows);
-        });
-      } //END showUsers()
+// Get the database of user names ready
+$("#btnShowNames").on("click", showUsers);
+function showUsers() {
+  db.allDocs({"include_docs" : true, "ascending" : true}, function(error, result) {
+    showTableOfUsers(result.rows);
+  });
+} //END showUsers()
       
-      // Display the user names in a table
-      function showTableOfUsers(data) {
-        var str  = "<p><table border='1' id='userTable'>";
-          str += "<tr><th>Time</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
-            for(var i = 0; i < data.length; i++) {
-              str += "<tr><td>" + data[i].id + 
-                "</td><td>" + data[i].doc.nameFirst + 
-                "</td><td>" + data[i].doc.nameLast + 
-                "</td><td>" + data[i].doc.nameEmail 
-                "</td></tr>";
-            }
-          str += "</table></p>";
+// Display the user names in a table
+function showTableOfUsers(data) {
+  var str  = "<p><table border='1' id='userTable'>";
+    str += "<tr><th>Time</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Location</th></tr>";
+      for(var i = 0; i < data.length; i++) {
+        str += "<tr><td>" + data[i].id + 
+          "</td><td>" + data[i].doc.nameFirst + 
+          "</td><td>" + data[i].doc.nameLast + 
+          "</td><td>" + data[i].doc.nameEmail + 
+          "</td><td>" + data[i].doc.userLocation + 
+          "</td></tr>";
+      }
+    str += "</table></p>";
           
          
-        $("#divResults").html(str);
-      } //END showTableOfUsers();
+  $("#divResults").html(str);
+} //END showTableOfUsers();
+$("#btnUpdateLocations").on("click", updateLocation);
+function updateLocation(){
+  db.get(Time).then(function(doc){
+   return db.put({
+      _id : doc._id,
+      _rev : doc._rev,
+      nameFirst : doc.nameFirst,
+      nameLast : doc.nameLast,
+      nameEmail : doc.nameEmail,
+      userLocation : doc.userLocation + "<br>" + $("#choose_location").val(),
+    });
+    }).catch(function (err) {
+      console.log(err);
+  })
+}
+
+
+
+
+
+// $("#btnUpdateLocations").on("click", updateLocation);
+// function updateLocation(){
+//   db.get(Time).then(function(doc){
+//    return db.put({
+//       _id : doc._id,
+//       _rev : doc._rev,
+//       nameFirst : doc.nameFirst,
+//       nameLast : doc.nameLast,
+//       nameEmail : doc.nameEmail,
+//       userLocation : doc.userLocation + "<br>" + $("#choose_location").val(),
+//     });
+//     }).catch(function (err) {
+//       console.log(err);
+//   })
+// }
+
+
+
+// var db;
+//       // Initialize PouchDB database
+//       function initDB() {
+//         db = new PouchDB("practice_details");
+//         db.info(function(error, result) {
+//           if(!error) {
+//             console.log(result);
+//           } else {
+//             console.log(error);
+//           }
+//         });
+//         return db;
+//       } //END initDB()
       
+//       initDB();
       
+//       // Functionality to add a user
+//       $("#btnAddNames").on("click", addUsers);
+//       function addUsers() {         
+//         var uFirst   = $("#inputFirst").val(),
+//           uLast  = $("#inputLast").val(),
+//           uEmail  = $("#inputEmail").val();
+//           uTime = new Date().toISOString();
+//         var aUser = {
+//           "_id"   : uTime,
+//           "nameFirst" : uFirst,
+//           "nameLast" : uLast,
+//           "nameEmail"  : uEmail
+//         };
+        
+//         db.put(aUser, function(error, result) {
+//           if(!error) {
+//             $("#divResults").html("<p>Saved!</p>");
+//             showUsers();
+//             console.log(result);
+//             clearFields();
+//           } else {
+//             $("#divResults").html("<p>&iexcl;Error!</p>");
+//             console.log(error);
+//           }
+//          db.replicate.to('https://minedmindstestdb:minedmindstestdb123@minedmindstestdb.cloudant.com/info_details');
+ 
+//         });
+//       } //END addUsers();
+//       // var remoteCouch = 'https://minedmindstestdb:minedmindstestdb123@minedmindstestdb.cloudant.com/info_details';
+      
+//       // To clear fields
+//       function clearFields() {
+//         $("#formUser")[0].reset();
+//       } //END clearFields()
+      
+//       // Get the database of user names ready
+//       $("#btnShowNames").on("click", showUsers);
+//       function showUsers() {
+//         db.allDocs({"include_docs" : true, "ascending" : true}, function(error, result) {
+//           showTableOfUsers(result.rows);
+//         });
+//       } //END showUsers()
+      
+//       // Display the user names in a table
+//       function showTableOfUsers(data) {
+//         var str  = "<p><table border='1' id='userTable'>";
+//           str += "<tr><th>Time</th><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+//             for(var i = 0; i < data.length; i++) {
+//               str += "<tr><td>" + data[i].id + 
+//                 "</td><td>" + data[i].doc.nameFirst + 
+//                 "</td><td>" + data[i].doc.nameLast + 
+//                 "</td><td>" + data[i].doc.nameEmail 
+//                 "</td></tr>";
+//             }
+//           str += "</table></p>";
+          
+         
+//         $("#divResults").html(str);
+//       } //END showTableOfUsers();
+      
+//       
